3e3a723c79b2cc979b1ae788ca310f785fd2af44
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-03-09 22:35:05 +0300

    feat: refactor date calculation functions and update input handling in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -10,8 +10,12 @@ const end = {
   year: document.querySelector("#endYear"),
 };
 
+const currentDate = document.querySelector("#current-date");
+const workingDate = document.querySelector("#working-date");
+
 /* calculating start/end dates in miliseconds */
-const getInputValues = () => {
+
+export const getInputValues = () => {
   const sDay = +start.day.value;
   const sMonth = +start.month.value;
   const sYear = +start.year.value;
@@ -20,21 +24,63 @@ const getInputValues = () => {
   const eMonth = +end.month.value;
   const eYear = +end.year.value;
 
-  const startDate = new Date(sYear, sMonth - 1, sDay);
-  const endDate = new Date(eYear, eMonth - 1, eDay);
+  const startDate = new Date(sYear, sMonth - 1, sDay).setHours(0, 0, 0, 0);
+  let endDate = new Date(eYear, eMonth - 1, eDay).setHours(0, 0, 0, 0);
+
+  if (currentDate.checked) {
+    endDate = new Date().setHours(0, 0, 0, 0);
+  }
 
   return { startDate, endDate };
 };
 
 /* calculating the deffrance between the 2 dates in miliseconds */
 
-const calculateDifference = ({ startDate, endDate }) => {
+export const calculateDifference = ({ startDate, endDate }) => {
   const diff = endDate - startDate;
 
   return { diff };
 };
-const claculateDates = ({ diff }) => {
-  const totalDays = diff / (1000 * 60 * 60 * 24);
+
+/* claculating total days  */
+export const calculateDates = ({ diff }) => {
+  let totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  if (workingDate.checked) {
+    totalDays += 1;
+  }
 
   return { totalDays };
+};
+
+/* geting the end-results */
+
+export const result = ({ totalDays }) => {
+  const years = Math.floor(totalDays / 365);
+  const months = Math.floor((totalDays % 365) / 30);
+  const days = Math.floor((totalDays % 365) % 30);
+
+  const inYears = totalDays / 365;
+  const inMonths = totalDays / 30;
+  const inDays = totalDays;
+
+  return { years, months, days, inYears, inMonths, inDays };
+};
+
+const resultOutput = document.querySelectorAll(".result");
+
+export const displayResult = ({
+  years,
+  months,
+  days,
+  inYears,
+  inMonths,
+  inDays,
+}) => {
+  resultOutput[0].textContent = years;
+  resultOutput[1].textContent = months;
+  resultOutput[2].textContent = days;
+  resultOutput[3].textContent = inYears;
+  resultOutput[4].textContent = inMonths;
+  resultOutput[5].textContent = inDays;
 };
