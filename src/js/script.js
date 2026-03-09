27923fd3718725b3106bd2ee5e6a491f21c39f20
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-03-09 19:53:10 +0300

    fix: correct header title in index.html and refactor date calculation logic in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -10,17 +10,31 @@ const end = {
   year: document.querySelector("#endYear"),
 };
 
-const sDay = +start.day.value;
-const sMonth = +start.month.value;
-const sYear = +start.year.value;
+/* calculating start/end dates in miliseconds */
+const getInputValues = () => {
+  const sDay = +start.day.value;
+  const sMonth = +start.month.value;
+  const sYear = +start.year.value;
 
-const eDay = +end.day.value;
-const eMonth = +end.month.value;
-const eYear = +end.year.value;
+  const eDay = +end.day.value;
+  const eMonth = +end.month.value;
+  const eYear = +end.year.value;
 
-const startDate = new Date(sYear, sMonth - 1, sDay);
-const endDate = new Date(eYear, eMonth - 1, eDay);
+  const startDate = new Date(sYear, sMonth - 1, sDay);
+  const endDate = new Date(eYear, eMonth - 1, eDay);
 
-document.querySelector(".button").addEventListener("click", calculateDuration);
+  return { startDate, endDate };
+};
 
-const calculateDuration = () => {};
+/* calculating the deffrance between the 2 dates in miliseconds */
+
+const calculateDifference = ({ startDate, endDate }) => {
+  const diff = endDate - startDate;
+
+  return { diff };
+};
+const claculateDates = ({ diff }) => {
+  const totalDays = diff / (1000 * 60 * 60 * 24);
+
+  return { totalDays };
+};
